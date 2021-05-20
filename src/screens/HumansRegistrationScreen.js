d2bdb6e2ccb9abb4362ed39ae2800a4a727e0e74
Abraomukas <abraomukas@gmail.com>
2021-05-20 19:52:42 +0400

    modyfying RegistrationScreen.js

--- src/screens/HumansRegistrationScreen.js
+++ src/screens/HumansRegistrationScreen.js
@@ -7,82 +7,154 @@ const formStyle = {
 
 function HumansRegistrationScreen() {
 
+    // initial - sending - successful - unsuccessful - validation error
     const [state, setState] = useState("initial");
-    
+
+    let avatarField;
+    let nameField;
+    let surnameField;
+    let emailField;
+    let passwordField;
+    let contactField;
+    let addressField;
+    let likesField;
+    let hatesField;
+
+
+    const register = () => {
+
+        const errors = [];
+
+
+        // validate the fields
+        if (nameField.value.length == 0) {
+            errors.push("You forgot to enter your name!");
+        }
+
+        if (surnameField.value.length == 0) {
+            errors.push("You forgot to enter your surname!");
+        }
+
+        if (emailField.value.length == 0) {
+            errors.push("Use a legit email!");
+        }
+
+        if (passwordField.value.length == 0) {
+            errors.push("Enter a stronger password!");
+        }
+
+        // if there are errors >>> validation error
+        if (errors.length > 0) {
+            setState("validation-error");
+        }
+
+        // if there are no errors >>> sending
+        else {
+            setState("sending");
+
+            fetch("http://localhost:3001/humans/find")
+                // if the submission is successful >>> successful
+                .then((backendResponse) => { backendResponse.json(); })
+                .then((theJson) => { console.log(theJson); })
+                // if the submission is not successful >>> unsuccessful
+                .catch((error) => { console.log(error); });
+        }
+
+
+
+    }
+
     return (
         <div>
-            <h1 className="py-5">Registration for Humans</h1>
 
             <div className="container" style={formStyle}>
 
+                <h1 className="py-5">Registration for Humans</h1>
 
                 {/* ### Avatar ### */}
                 <div className="mb-3">
                     <label for="avatar" className="form-label">Avatar</label>
-                    <input type="input" className="form-control" id="avatar" aria-describedby="avatarHelp" />
+                    <input ref={(elem) => { avatarField = elem }} type="input" className="form-control" id="avatar" aria-describedby="avatarHelp" />
                     <div id="avatarHelp" className="form-text">Pic something cool you identify yourself with</div>
                 </div>
 
                 {/* ### Name ### */}
                 <div className="mb-3">
                     <label for="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" aria-describedby="nameHelp" />
+                    <input ref={(elem) => { nameField = elem }} type="text" className="form-control" id="name" aria-describedby="nameHelp" />
                     <div id="nameHelp" className="form-text">How do you parents call you?</div>
                 </div>
 
                 {/* ### Surname ### */}
                 <div className="mb-3">
                     <label for="surname" className="form-label">Surname</label>
-                    <input type="text" className="form-control" id="surname" aria-describedby="surnameHelp" />
+                    <input ref={(elem) => { surnameField = elem }} type="text" className="form-control" id="surname" aria-describedby="surnameHelp" />
                     <div id="surnameHelp" className="form-text">What about family names?</div>
                 </div>
 
                 {/* ### Email ### */}
                 <div className="mb-3">
                     <label for="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                    <input ref={(elem) => { emailField = elem }} type="email" className="form-control" id="email" aria-describedby="emailHelp" />
                     <div id="emailHelp" className="form-text">Spam? No thank you!</div>
                 </div>
 
                 {/* ### Password ### */}
                 <div className="mb-3">
                     <label for="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" aria-describedby="passwordHelp" />
+                    <input ref={(elem) => { passwordField = elem }} type="password" className="form-control" id="password" aria-describedby="passwordHelp" />
                     <div id="passwordHelp" className="form-text">Please do NOT use "password123"</div>
                 </div>
 
                 {/* ### Contact nuber ### */}
                 <div className="mb-3">
                     <label for="contact" className="form-label">Contact number</label>
-                    <input type="contact" className="form-control" id="contact" aria-describedby="contactHelp" />
+                    <input ref={(elem) => { contactField = elem }} type="contact" className="form-control" id="contact" aria-describedby="contactHelp" />
                     <div id="contactHelp" className="form-text">Remember phone calls?</div>
                 </div>
 
                 {/* ### Address ### */}
                 <div className="mb-3">
                     <label for="address" className="form-label">Address</label>
-                    <input type="contact" className="form-control" id="address" aria-describedby="addressHelp" />
+                    <input ref={(elem) => { addressField = elem }} type="contact" className="form-control" id="address" aria-describedby="addressHelp" />
                     <div id="addressHelp" className="form-text">Where do we send our appreciation tokens?</div>
                 </div>
 
                 {/* ### Likes ### */}
-                {/* ### Does not like ### */}
+                {/* ### Hates ### */}
 
-                <button className="btn btn-success" type="button">Submit</button>
+                {
+                    state !== "loading" && state !== "successful" &&
+                    <button
+                        onClick={register}
+                        className="btn btn-success mb-5" type="button">Submit</button>
+                }
 
-                <div className="alert alert-danger" role="alert">
-                    Please enter correct  values
-                </div>
+                {
+                    state === "validation-error" &&
+                    <div className="alert alert-danger" role="alert">
+                        Please enter correct  values
+                    </div>
+                }
 
-                <div className="alert alert-success" role="alert">
-                    SUCCESS! Welcome aboard!
-                </div>
+                {
+                    state === "successful" &&
+                    <div className="alert alert-success" role="alert">
+                        SUCCESS! Welcome aboard!
+                    </div>
+                }
 
-                <div className="alert alert-warning" role="alert">
-                    Something is not working on our end...
-                </div>
+                {
+                    state === "unsuccessful" &&
+                    <div className="alert alert-warning" role="alert">
+                        Something is not working on our end...
+                    </div>
+                }
 
-                <p>Hold on a minute...</p>
+                {
+                    state === "sending" &&
+                    <p>Hold on a minute...</p>
+                }
 
             </div>
 
